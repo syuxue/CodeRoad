60098f7a0077401e075ed0df7c61824cf5bf331a
ShiNey <syuxue@hotmail.com>
2013-07-29 07:28:19 +0800

    1.3.8 Tried

--- Cp1/1.3.8/1.38.cpp
+++ Cp1/1.3.8/1.38.cpp
@@ -4,10 +4,10 @@ For 1.38. Overflow the stack.
 
 #include "stdio.h"
 #include "stdlib.h"
-static int i;			//如何可以计算出栈深度?貌似这样每次出来的值都不一样.
+int i=0;			//如何可以计算出栈深度?貌似这样每次出来的值都不一样.
 
 int next(int num){
-	i=num;
+	i++;
 	return next(num+1);
 }
 
